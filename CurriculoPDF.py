f213d1f011381c3309501eb662b22b042f6d6df1
= <allanwelerson@gmail.com>
2019-03-11 01:06:04 -0300

    adicionar funcionalidades e separar  arquivo css do index.html para arquivo proprio

--- CurriculoPDF.py
+++ CurriculoPDF.py
@@ -7,6 +7,8 @@ def gerarPDF(pdf, dados):
     pdf.set_font('Arial', '', 12)
     pdf = dadosPessoais(pdf, dados)
     pdf = formacao(pdf, dados)
+    pdf = experienciaProfissional(pdf, dados)
+    pdf = addObjetivo(pdf, dados)
 
 
 
@@ -84,7 +86,50 @@ def mostraGrau(pdf, dados):
     curso2_desc = dados['curso2_desc']
     curso2_instituicao = dados['curso2_instituicao']
 
-    if len(curso1_desc) == 0 :
+    if len(curso1_desc) == 0 and len(curso1_instituicao) == 0 or len(curso2_desc) == 0  and len(curso2_instituicao) == 0:
         pdf.cell(0, 10, dados['grau'] , 0, 1, 'L')
+
+    return pdf
+
+def experienciaProfissional(pdf, dados):
+
+    if temExperienciaProfissional(dados):
+
+        pdf.cell(0, 10, "Experiência Profissional", 1, 1, 'C')
+        pdf = addExperienciaProfissional(pdf, dados['exp1_funcao'], dados['exp1_empresa'])
+        pdf = addExperienciaProfissional(pdf, dados['exp2_funcao'], dados['exp2_empresa'])
+        pdf = addExperienciaProfissional(pdf, dados['exp3_funcao'], dados['exp3_empresa'])
+
+    return pdf
+
+
+
+def temExperienciaProfissional(dados):
+
+    exp1_f = dados['exp1_funcao']
+    exp1_emp = dados['exp1_empresa']
+    exp2_f = dados['exp2_funcao']
+    exp2_emp = dados['exp2_empresa']
+    exp3_f = dados['exp3_funcao']
+    exp3_emp = dados['exp3_empresa']
+
+    if len(exp1_emp) > 0 and len(exp1_f) > 0 or len(exp2_emp) > 0 and len(exp2_f) or len(exp3_emp) > 0 and len(exp3_f):
+        return True
+
+    return False
+
+def addExperienciaProfissional(pdf, funcao, empresa):
+    if len(funcao) > 0 and len(empresa) > 0:
+        exp = ' - ' + empresa + ' - ' + funcao
+        pdf.cell(0, 10, exp, 0, 1, 'L' )
+    return pdf
+
+
+def addObjetivo(pdf, dados):
+
+    if len(dados['objetivo']) > 0 :
+        pdf.cell(0, 10, "Objetivo", 1, 1, 'C')
+        pdf.multi_cell(0, 10, dados['objetivo'], 0, 1, 'C')
+
 
     return pdf
